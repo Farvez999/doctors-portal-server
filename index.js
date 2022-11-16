01dfef1ecb6b228129de991194dca07a7db6a19d
Farvez999 <engrfrzabdullah999@gmail.com>
2022-11-17 03:26:08 +0600

    Email query

--- index.js
+++ index.js
@@ -49,6 +49,14 @@ async function run() {
             res.send(options);
         });
 
+        // Booking find data by email
+        app.get('/bookings', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const bookings = await bookingsCollection.find(query).toArray();
+            res.send(bookings)
+        })
+
         // bookings post (submit data)
         app.post('/bookings', async (req, res) => {
             const booking = req.body;
