e317cfba9bdbe51194b8be48de4736b08409fa1c
Farvez999 <engrfrzabdullah999@gmail.com>
2022-11-18 00:22:29 +0600

    Create Make admin API limit the feature based on admin level

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
@@ -119,10 +119,44 @@ async function run() {
             res.status(403).send({ accessToken: '' })
         });
 
+        //All User 
+        app.get('/users', async (req, res) => {
+            const query = {}
+            const users = await usersCollection.find(query).toArray()
+            res.send(users)
+        })
+
         //User post
         app.post('/users', async (req, res) => {
             const user = req.body;
             const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
+
+        // Update user role Admin
+        app.put('/users/admin/:id', verifyJWT, async (req, res) => {
+
+
+            const decodedEmail = req.decoded.email;
+            const query = { email: decodedEmail };
+            const user = await usersCollection.findOne(query);
+
+            if (user?.role !== 'admin') {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+
+
+            const id = req.params.id;
+            console.log(id)
+            const filter = { _id: ObjectId(id) }
+            console.log(filter)
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await usersCollection.updateOne(filter, updatedDoc, options);
             res.send(result);
         })
 
