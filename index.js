6d4c629f4c93f1fb5873bba474f65fb71f103c7d
Farvez999 <engrfrzabdullah999@gmail.com>
2022-11-18 01:29:39 +0600

    Prevent accessing Admin route via URL

--- index.js
+++ index.js
@@ -126,6 +126,14 @@ async function run() {
             res.send(users)
         })
 
+        // User get Admin permistion
+        app.get('/users/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email }
+            const user = await usersCollection.findOne(query);
+            res.send({ isAdmin: user?.role === 'admin' });
+        })
+
         //User post
         app.post('/users', async (req, res) => {
             const user = req.body;
